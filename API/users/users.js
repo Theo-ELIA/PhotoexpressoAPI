e5e83b93b977a19c5d5aca21e48f78fc2a96b9b9
Romain <romain@scaleflex.cloud>
2016-12-19 10:08:24 +0100

    changement sur les requetes sql

--- API/users/users.js
+++ API/users/users.js
@@ -40,8 +40,8 @@ router.get('/connection',function(req,res)
 
 router.get('/listOrders',function(req,res)
 {
-	var user_id = [1];
-	var query = "SELECT shipping_fee, last_name, first_name, street_adress, postal_code, city, gender, quantity, price_per_unit, date_purchase, date_delivery FROM purchase_per_adress pa JOIN adress ON pa.shipping_adress_id = adress.id JOIN exemplary_quantity eq ON eq.purchase_id = pa.id JOIN purchase_historic ph ON pa.global_purchase_id = ph.id WHERE pa.customer_id = $1";
+	var user_id = [4];
+	var query = "SELECT shipping_fee, last_name, first_name, street_adress, postal_code, city, gender, quantity, price_per_unit, date_purchase, date_delivery FROM purchase.purchase_per_adress pa JOIN users.adress ON pa.shipping_adress_id = adress.id JOIN purchase.exemplary_quantity eq ON eq.purchase_id = pa.id JOIN purchase.purchase_historic ph ON pa.global_purchase_id = ph.id WHERE ph.customer_id = $1";
 	database.connect(query, function(err,result) {
 		if(err)
 		{
@@ -80,7 +80,7 @@ router.get('/modifyAccount',function(req,res)
 
 router.get('/AdressList',function(req,res)
 {
-	var user_id = [1];
+	var user_id = [4];
 	var query = 'SELECT last_name, first_name, street_adress, postal_code, city, gender FROM contact_list cl LEFT JOIN adress ON cl.shipping_adress_id = adress.id WHERE cl.user_id = $1';
 	database.connect(query, function(err, result) {
 		if(err)
