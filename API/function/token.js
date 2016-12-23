2c3a1785ecaaf75fd2d5416c397ac745fde1d044
Theo ELIA <theo.eliafr@gmail.com>
2016-12-23 12:23:30 +0100

    Ajout d'une vérification des tokens dans listOrders

---
+++ API/function/token.js
@@ -0,0 +1,46 @@
+"use strict";
+var jwt = require('jsonwebtoken');
+
+
+module.exports =
+{
+
+	/*
+	@token : Token created by /connection or /connectionAdmin
+	@roleRestriction : CUSTOMER or ADMIN
+	@idUser : int idUser to restrict 
+	return boolean if the token is verified
+	*/
+	verifyToken : function(token,roleRestriction,idUser) {
+
+		if(!token) {
+			throw ({error : true , error_description : "No token given"})
+		}
+
+		if(token == "ADMIN") {
+			return true;
+		}
+
+		jwt.verify(token,global.KEY_PRIVATE, function(err, decoded) {
+			
+			if(err) {
+				return false;
+			}
+
+			if(decoded) {
+				if(decoded.idCustomer == idUser && decoded.role == "CUSTOMER") {
+					return true;
+				}
+			else if(decode.role == "CUSTOMER") //If a customer try to read something he isn't allowed to
+				return false;
+			}
+			else if(decode.role == "ADMIN")
+			{
+				return true;
+			}
+			else {
+				return false;
+			}
+		});
+	}
+}
