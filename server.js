41f568f39781d4bc8a2d3352473691ae2867fac3
Theo Elia <theo.elia@ucopia.com>
2017-01-02 18:11:24 +0100

    Ajout d'un log plus fin grâce à Winston

--- server.js
+++ server.js
@@ -1,10 +1,21 @@
 "use strict";
-//We import the library we need
-var express = require('express');
-var path = require("path");
-var httpProxy = require('http-proxy');
-var argv = require('optimist').argv;
 
+//We import the configuration from the configuration file
+var config = require('./config.json')[process.env.NODE_ENV || 'development'];
+
+//We import the library we need
+var express = require('express'); //Framework to create REST API
+var path = require("path"); //Module to generate Path to static files
+var httpProxy = require('http-proxy'); //Module to do reverse proxy
+var argv = require('optimist').argv; //Module to process Console arguments
+const winston = require('winston') // Module to manage logging
+//We create a customer Logger wich logg events to the console and Server.log
+var logger = new (winston.Logger)({
+	transports: [
+		new (winston.transports.Console)({colorize: true,}),
+		new (winston.transports.File)({ colorize: true, filename: './server.log' })
+	]
+});
 //We create our application
 var app = express();
 var bodyParser = require('body-parser');
@@ -29,6 +40,7 @@ else
 	port = 80
 }
 
+logger.level = config.logging;
 var hostname = '192.168.11.112';
 var allhostname = '0.0.0.0';
 
@@ -50,8 +62,6 @@ app.get('/report_error', function (req, res) {
 app.all("/phppgadmin/*", function(req, res) {
     apiProxy.web(req, res, {target: serverPHPPGAdmin});
 });
-
-
 
 app.listen(port || 8080, effectiveHost, function(){
   console.log('Server running at http://'+effectiveHost+':'+port+'/');
