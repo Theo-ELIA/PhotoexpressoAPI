35c8f538c41e1681151040a22d696034bc7eb435
Theo ELIA <theo.eliafr@gmail.com>
2017-02-03 12:17:10 +0100

    Refactorer les fichiers de configurations dans un dossier à part

--- API/function/database.js
+++ API/function/database.js
@@ -1,16 +1,6 @@
 "use strict";
 var pg = require('pg-promise')();
-//Take a callback function and a SQL query
-
-var SQLconfig =  {
-	user: 'admin',
-	database: 'photoexpresso',
-	password: 'admin',
-	port: 5432,
-	max: 10,
-	idleTimeoutMillis: 30000,
-};
-
+var SQLconfig = require('../../config/database.conf.js')
 var client =  pg(SQLconfig);
 
 module.exports =
@@ -24,13 +14,13 @@ module.exports =
 
 		console.log("Executing " + sqlQuery + " in database.js module");
 		return client.query(sqlQuery,arrayParameters)
-			.then(function(resultRow) {
+		.then(function(resultRow) {
 
 			console.log("Result rows found :");
 			console.log(resultRow);
 			return resultRow;
 		})
-			.catch( function(err) {
+		.catch( function(err) {
 			console.log("Fail to retrieve from the Database")
 			console.log(err);
 			return err
--- server.js
+++ server.js
@@ -1,55 +1,54 @@
 "use strict";
 
-//We import the configuration from the configuration file
-var config = require('./config.json')[process.env.NODE_ENV || 'development'];
-
 //We import the library we need
 var express = require('express'); //Framework to create REST API
 var path = require("path"); //Module to generate Path to static files
 var httpProxy = require('http-proxy'); //Module to do reverse proxy
 var argv = require('optimist').argv; //Module to process Console arguments
 const winston = require('winston') // Module to manage logging
-//We create a customer Logger wich logg events to the console and Server.log
+//We set the parameters of our application
+
+//Default Mode
+var mode = "debug"
+
+if(argv.mode) {
+	mode = argv.mode;
+}
+//We import the configuration from the configuration file
+var config = require('./config/application.conf.js').getConfiguration(mode);
+
+if(!config) {
+	process.exit(1);
+}
+
+
 var logger = new (winston.Logger)({
+	level : config.loggingLevel,
 	transports: [
 		new (winston.transports.Console)({colorize: true,}),
 		new (winston.transports.File)({ colorize: true, filename: './server.log' })
 	]
 });
+if(mode === 'debug') {
+	logger.debug('Running mode : ' + mode);
+}
+logger.debug(config);
+
+
+
 //We create our application
 var app = express();
 var bodyParser = require('body-parser');
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({ extended: true })); // support encoded bodies
 var apiProxy = httpProxy.createProxyServer();//We import the file we need
-
-//We parse the arguments of the script
-global.isDebugMode = (argv.debug != undefined);
-if(global.isDebugMode) {
-	console.log("Debug Mode enabled")
-}
 var apiPath = require('./API/api');
-var port
-
-if (argv.local)
-{
-	port = 8080
-}
-else
-{
-	port = 80
-}
-
-logger.level = config.logging;
-var hostname = '192.168.11.112';
-var allhostname = '0.0.0.0';
 
 var serverPHPPGAdmin = "http://127.0.0.1:8080"
-var effectiveHost = allhostname;
 
 app.use('/outTemp/', express.static(__dirname+'/outTemp'))
 app.use('/API/',apiPath)
-//app.use('/pictures/',picturesRoute)
+
 
 app.get('/', function (req, res) {
 	res.sendFile(path.join(__dirname+'/public/html/index.html'));
@@ -63,6 +62,6 @@ app.all("/phppgadmin/*", function(req, res) {
     apiProxy.web(req, res, {target: serverPHPPGAdmin});
 });
 
-app.listen(port || 8080, effectiveHost, function(){
-  console.log('Server running at http://'+effectiveHost+':'+port+'/');
+app.listen(config.port || 8080, config.listening_interface, function(){
+  console.log('Server running at http://'+config.listening_interface+':'+config.port+'/');
 });
